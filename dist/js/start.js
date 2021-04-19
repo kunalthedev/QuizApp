10d7a60d47dd1c94c341edee6ea05b773d95c897
Kunal Arora <10kunalji@gmail.com>
2021-04-19 20:51:56 +0530

    done

--- dist/js/start.js
+++ dist/js/start.js
@@ -1,10 +1,10 @@
 function submitForm(e) {
     e.preventDefault();
     let name = document.forms["welcome_form"]["name"].value;
-    let email = document.forms["welcome_form"]["email"].value;
+    // let email = document.forms["welcome_form"]["email"].value;
   
     sessionStorage.setItem("name", name);
-    sessionStorage.setItem("email", email);
+    // sessionStorage.setItem("email", email);
   
     location.href = "instruction.html";
   }
